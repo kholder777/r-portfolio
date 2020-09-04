9db9e2b287d4d5758d72438661db9e27be587a67
Kevin Holder <kholder777@yahoo.com>
2020-09-03 22:36:47 -0500

    re-engaged some jQ features for the navbar.

--- public/js/init.js
+++ public/js/init.js
@@ -79,21 +79,21 @@
 // 	/*	Fade In/Out Primary Navigation
 // ------------------------------------------------------*/
 
-// 	$(window).on("scroll", function () {
-// 		var h = $("header").height();
-// 		var y = $(window).scrollTop();
-// 		var nav = $("#nav-wrap");
+$(window).on("scroll", function () {
+	var h = $("header").height();
+	var y = $(window).scrollTop();
+	var nav = $("#nav-wrap");
 
-// 		if (y > h * 0.2 && y < h && $(window).outerWidth() > 768) {
-// 			nav.fadeOut("fast");
-// 		} else {
-// 			if (y < h * 0.2) {
-// 				nav.removeClass("opaque").fadeIn("fast");
-// 			} else {
-// 				nav.addClass("opaque").fadeIn("fast");
-// 			}
-// 		}
-// 	});
+	if (y > h * 0.2 && y < h && $(window).outerWidth() > 768) {
+		nav.fadeOut("fast");
+	} else {
+		if (y < h * 0.2) {
+			nav.removeClass("opaque").fadeIn("fast");
+		} else {
+			nav.addClass("opaque").fadeIn("fast");
+		}
+	}
+});
 
 // 	/*----------------------------------------------------*/
 // 	/*	Modal Popup
